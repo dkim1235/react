3cf8b2febbc7d7524c695bd4b752ae3ccb304cf1
dkim1235 <dkim1235@gmail.com>
2023-04-19 17:07:36 -0700

    create new project that is replicating what I've already done

---
+++ practice-project-c-solo/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import AddUser from './components/User/index';
+
+function App() {
+  return (
+    <div className="App">
+      <AddUser>
+      </AddUser>
+
+    </div>
+  );
+}
+
+export default App;
