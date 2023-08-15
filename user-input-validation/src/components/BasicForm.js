166fc0150ed14426d020eaedee52df63c87a7902
dkim1235 <dkim1235@gmail.com>
2023-08-15 16:19:42 -0700

    try to do the basic form validation individually. make notes of all mistakes

--- user-input-validation/src/components/BasicForm.js
+++ user-input-validation/src/components/BasicForm.js
@@ -1,21 +1,45 @@
+import { useState } from 'react';
+
 const BasicForm = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+	const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
+
+	const enteredNameIsValid = enteredName.trim() !== '';
+	const enteredNameHasError = !enteredNameIsValid && enteredNameIsTouched;
+
+  const enteredNameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+	const enteredNameIsTouchedHandler = (event) => {
+		setEnteredNameIsTouched(true);
+	}
+
+	const nameInputClasses = enteredNameHasError ? 'form-control invalid' : 'form-control';
+
   return (
     <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+      <div className="control-group">
+        <div className={nameInputClasses}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={enteredNameChangeHandler}
+            value={enteredName}
+						onBlur={enteredNameIsTouchedHandler}
+          />
+					{enteredNameHasError && (<p className="error-text">the entered name is Invalid</p>)}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
