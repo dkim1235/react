19f259881e7d95669f418c803d71414a8145a385
dkim1235 <dkim1235@gmail.com>
2023-08-16 16:21:33 -0700

    add the validation for the last name and email. install vim plugin for visual studio

--- user-input-validation/src/components/BasicForm.js
+++ user-input-validation/src/components/BasicForm.js
@@ -1,43 +1,103 @@
 import { useState } from 'react';
 
 const BasicForm = (props) => {
-  const [enteredName, setEnteredName] = useState('');
-	const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
+  const [enteredFirstName, setEnteredFirstName] = useState('');
+  const [enteredFirstNameIsTouched, setEnteredFirstNameIsTouched] =
+    useState(false);
 
-	const enteredNameIsValid = enteredName.trim() !== '';
-	const enteredNameHasError = !enteredNameIsValid && enteredNameIsTouched;
+  const enteredFirstNameIsValid = enteredFirstName.trim() !== '';
+  const enteredFirstNameHasError =
+    !enteredFirstNameIsValid && enteredFirstNameIsTouched;
 
-  const enteredNameChangeHandler = (event) => {
-    setEnteredName(event.target.value);
+  const [enteredLastName, setEnteredLastName] = useState('');
+  const [enteredLastNameIsTouched, setEnteredLastNameIsTouched] =
+    useState(false);
+
+  const enteredLastNameIsValid = enteredLastName.trim() !== '';
+  const enteredLastNameHasError =
+    !enteredLastNameIsValid && enteredLastNameIsTouched;
+
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmailIsTouched, setEnteredEmailIsTouched] = useState(false);
+
+  const enteredEmailIsValid = enteredEmail.includes('@');
+  const enteredEmailHasError = !enteredEmailIsValid && enteredEmailIsTouched;
+
+  const enteredFirstNameChangeHandler = (event) => {
+    setEnteredFirstName(event.target.value);
   };
-	const enteredNameIsTouchedHandler = (event) => {
-		setEnteredNameIsTouched(true);
-	}
+  const enteredFirstNameIsTouchedHandler = (event) => {
+    setEnteredFirstNameIsTouched(true);
+  };
 
-	const nameInputClasses = enteredNameHasError ? 'form-control invalid' : 'form-control';
+  const enteredLastNameChangeHandler = (event) => {
+    setEnteredLastName(event.target.value);
+  };
+  const enteredLastNameIsTouchedHandler = (event) => {
+    setEnteredLastNameIsTouched(true);
+  };
+
+  const enteredEmailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+  const enteredEmailIsTouchedHandler = (event) => {
+    setEnteredEmailIsTouched(true);
+  };
+
+  const firstNameInputClasses = enteredFirstNameHasError
+    ? 'form-control invalid'
+    : 'form-control';
+  const lastNameInputClasses = enteredLastNameHasError
+    ? 'form-control invalid'
+    : 'form-control';
+  const emailInputClasses = enteredEmailHasError
+    ? 'form-control invalid'
+    : 'form-control';
 
   return (
     <form>
       <div className="control-group">
-        <div className={nameInputClasses}>
+        <div className={firstNameInputClasses}>
           <label htmlFor="name">First Name</label>
           <input
             type="text"
             id="name"
-            onChange={enteredNameChangeHandler}
-            value={enteredName}
-						onBlur={enteredNameIsTouchedHandler}
+            onChange={enteredFirstNameChangeHandler}
+            value={enteredFirstName}
+            onBlur={enteredFirstNameIsTouchedHandler}
           />
-					{enteredNameHasError && (<p className="error-text">the entered name is Invalid</p>)}
+          {enteredFirstNameHasError && (
+            <p className="error-text">the entered first name is Invalid</p>
+          )}
         </div>
-        <div className="form-control">
+        <div className={lastNameInputClasses}>
           <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            onChange={enteredLastNameChangeHandler}
+            value={enteredLastName}
+            onBlur={enteredLastNameIsTouchedHandler}
+          />
+          {enteredLastNameHasError && (
+            <p className="error-text">the entered last name is invalid</p>
+          )}
         </div>
       </div>
       <div className="form-control">
-        <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" />
+        <div className={emailInputClasses}>
+          <label htmlFor="name">E-Mail Address</label>
+          <input
+            type="text"
+            id="name"
+            onChange={enteredEmailChangeHandler}
+            value={enteredEmail}
+            onBlur={enteredEmailIsTouchedHandler}
+          />
+          {enteredEmailHasError && (
+            <p className="error-text">the entered email is invalid</p>
+          )}
+        </div>
       </div>
       <div className="form-actions">
         <button>Submit</button>
