5140cdfe1a9c2e0b494504d91f14c466a953b828
dkim1235 <dkim1235@gmail.com>
2023-09-04 11:43:53 -0700

    take notes on solo integraition of forms and fetching database

---
+++ food-order-app-sec18-form-db/README.notes.txt
@@ -0,0 +1,24 @@
+1. create an order form
+	- first name
+	- last name
+	- email
+	- address
+	- zipcode
+	- city
+	- state
+2. create dummy data in firebase - done
+3. fetch data from dummy database
+4. loop around fetched data to print meals in the form expected.
+5. how does fetch tasks interact with useEffect and the useHttp function?
+	- the use Http function requires an object where a url can be 
+6. understand how the async function works - done
+	https://stackoverflow.com/questions/42964102/syntax-for-an-async-arrow-function
+7. how does the fetch function work? - done
+	- https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+2. submit orders to a BE server.
+	- in this case, firebase is used. 
+	- is it worth looking into postgres? 
+
+3. fetch meals data
+	- from the dummy meals, fetch the meals from the BE server
+	
---
+++ food-order-app-sec18-form-db/src/App.js
@@ -0,0 +1,59 @@
+import React, { useCallback, Fragment, useState } from 'react';
+
+import Header from './components/Layouts/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const [meals, setMeals] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      applyData(data);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  }, []);
+
+  sendRequest(
+    { url: 'https://react-http-e52a9-default-rtdb.firebaseio.com/' },
+    dummyApplyData
+  );
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <Meals></Meals>
+    </CartProvider>
+  );
+}
+
+export default App;
